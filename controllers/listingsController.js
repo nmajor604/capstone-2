c34817fd6681a0e17b8c04e08ba013ec222f575e
nmajor604 <81140580+nmajor604@users.noreply.github.com>
2022-12-15 00:00:30 -0800

    Updated listingController

--- controllers/listingsController.js
+++ controllers/listingsController.js
@@ -2,7 +2,7 @@ const knex = require('knex')(require('../knexfile').development);
 
 exports.index = (_req, res) => {
   knex('listings')
-    .where({ is_sold: '0' })
+    .where({ is_sold: '1' })
     .then((data) => {
       res.status(200).json(data);
     })
